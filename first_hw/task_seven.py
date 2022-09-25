7327e68e750967b55aa3fb9da1e2628d048304c3
misha1potapenko <misha1potapenko@yandex.ru>
2022-09-25 12:33:13 +0300

    added solution task 7

--- first_hw/task_seven.py
+++ first_hw/task_seven.py
@@ -1,2 +1,11 @@
 # 7.	Напишите программу для. проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+print("x y z f_1  f_second")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            f_first = not(x or y or z)
+            f_second = not(x) and not(y) and not(z)
+            print(x, y, z, bool(f_first), bool(f_second))
+
