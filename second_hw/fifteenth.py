8c0010c75a52d0f1f51f3812ae6ed14b1b35b227
misha1potapenko <misha1potapenko@yandex.ru>
2022-09-26 16:45:37 +0300

    added solution task 15

--- second_hw/fifteenth.py
+++ second_hw/fifteenth.py
@@ -3,3 +3,11 @@
 # Пример:
 # o	пусть N = 4, тогда [ 1, 2, 6, 24 ]\
 #     (1, 1*2, 1*2*3, 1*2*3*4)
+
+numbers = int(input("Введите число "))
+list = []
+multiplication = 1
+for i in range(1, numbers + 1):
+    list.append(multiplication * i)
+    multiplication = multiplication * i
+print(list)
