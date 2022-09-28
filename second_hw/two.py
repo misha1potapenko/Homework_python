fb8939b9bd62c5f43791332338594c569027882c
misha1potapenko <misha1potapenko@yandex.ru>
2022-09-28 16:50:45 +0300

    added function factorial

--- second_hw/two.py
+++ second_hw/two.py
@@ -4,10 +4,16 @@
 # o	пусть N = 4, тогда [ 1, 2, 6, 24 ]\
 #     (1, 1*2, 1*2*3, 1*2*3*4)
 
-numbers = int(input("Введите число "))
-list = []
-multiplication = 1
-for i in range(1, numbers + 1):
-    list.append(multiplication * i)
-    multiplication = multiplication * i
-print(list)
+
+def factorial(num):
+    numbers = int(input("Введите число "))
+    list = []
+    multiplication = 1
+    for i in range(1, numbers + 1):
+        list.append(multiplication * i)
+        multiplication = multiplication * i
+    print(list)
+
+
+factorial(5)
+
