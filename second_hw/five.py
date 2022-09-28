ac1e3a43d7c2cf2d72bc04ac7018f5d7236ab02a
misha1potapenko <misha1potapenko@yandex.ru>
2022-09-28 20:04:27 +0300

    added solution but not good

--- second_hw/five.py
+++ second_hw/five.py
@@ -1,2 +1,19 @@
 # Задание 5 Реализуйте алгоритм перемешивания списка.
 
+import random
+
+rand_list = []
+n = 10
+for i in range(n):
+    rand_list.append(random.randint(1, 10))
+print(rand_list)
+
+
+def shake_list(rand_list):
+    for i in range(0, 10):
+        rand_list[i] = rand_list[random.randrange(1, 10)]
+    print(rand_list)
+
+
+shake_list(rand_list)
+
